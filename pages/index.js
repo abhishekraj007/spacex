31b38ac37989d1f1a01e832fe7892bdfd50a9b6e
Abhishekraj <abhishekraj@Anoops-Macbook-Pro.local>
2020-08-05 07:50:00 +0530

    Added comments in code

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,17 @@ import Filter from "../components/Filter";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
+/*
+ * Home page component:
+ * Its contains: Head, Header, Filter, ListItems, Footer
+ *
+ * For ListItems: Render items for first time through props
+ * which is available throgh getServerSideProps method in nextjs
+ *
+ * On Filter change get latest data from api
+ * And store in state to re-render component with latest data
+ */
+
 class Home extends React.Component {
   constructor(props) {
     super(props);
@@ -18,8 +29,12 @@ class Home extends React.Component {
     };
   }
 
+  // On each filter change this method gets called with respective
+  // applied filter value
   handleFilterChange = async (filterValue) => {
     const { year, launch_success, land_success } = filterValue;
+
+    // Compose query based on applied filter
     let query = "?limit=50";
     if (year !== null) query = `${query}&launch_year=${year}`;
     if (launch_success !== null)
@@ -27,8 +42,10 @@ class Home extends React.Component {
     if (land_success !== null) query = `${query}&land_success=${land_success}`;
     this.setState({ loading: true });
 
+    // Get data using this query
     let result = await fetchData(query);
 
+    // Set latest data into state to re-render this component
     this.setState(
       {
         programs: result,
@@ -36,11 +53,13 @@ class Home extends React.Component {
         loading: false,
       },
       () => {
+        // Change the URL with latest query
         this.props.router.push(query, query, { shallow: true });
       }
     );
   };
 
+  // To clear filter
   handleClearFilter = () => {
     this.setState(
       {
@@ -48,6 +67,8 @@ class Home extends React.Component {
         filterApplied: false,
       },
       () => {
+        // after filter is cleared
+        // clear the URL
         this.props.router.push("/", "/", { shallow: true });
       }
     );
@@ -74,16 +95,23 @@ class Home extends React.Component {
 
     return (
       <div className="page-wrapper">
+        {/* Cutomized Head for SEO coverage */}
         <CustomHead />
+
+        {/* Header component with Logo */}
         <Header />
+
+        {/* Main Container */}
         <main className="main">
           <div className="home-container">
+            {/* Filter View  */}
             <Filter
               className="filter-view"
               handleFilterChange={this.handleFilterChange}
               handleClearFilter={this.handleClearFilter}
             />
 
+            {/* List View  */}
             <div className="list-view programs-list">
               {loading ? (
                 <div className="loader">
@@ -95,15 +123,16 @@ class Home extends React.Component {
           </div>
         </main>
 
+        {/* Footer Component  */}
         <Footer />
       </div>
     );
   }
 }
 
-// This is metod is get called once on server
+// This is methods gets called on server on each browser refresh, only once.
 export async function getServerSideProps() {
-  // Do first render by server side
+  // Does first rendering cycle through server
   // Get data from api and return it
 
   const limit = "?limit=50";
